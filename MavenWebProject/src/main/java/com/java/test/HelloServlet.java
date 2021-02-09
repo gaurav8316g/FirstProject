546ec5b96a905e8cf82ae30c583fab8593a3fa2e
Gaurav.Goyal <Gaurav.Goyal@host.docker.internal>
2021-02-09 09:33:25 -0500

    first commit

---
+++ MavenWebProject/src/main/java/com/java/test/HelloServlet.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.java.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Gaurav.Goyal
+ *
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	resp.setContentType("text/plain");
+	resp.getWriter().write("Maven web project deployed on Tomcat Project!");
+	}
+
+}
